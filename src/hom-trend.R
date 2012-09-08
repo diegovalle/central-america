c1777e485557d8e75cfbd5e3d5b36e5ff773ba0e
Diego Valle-Jones <diegovalle@gmail.com>
2012-09-08 11:56:55 -0500

    time series as a percentage of 2006

--- src/hom-trend.R
+++ src/hom-trend.R
@@ -16,17 +16,34 @@ direct.label(p, "last.bumpup")
 ggsave("graphs/hom-ca.png", dpi = 100,
        width = 9, height = 5)
 
-hom.ca <- subset(hom.ca, Year >= 2000)
-hom.ca <- ddply(hom.ca, .(Country.or.Area), transform,
+hom.ca.per <- subset(hom.ca, Year >= 2000)
+hom.ca.per <- ddply(hom.ca.per, .(Country.or.Area), transform,
       per = Rate / Rate[length(Rate)])
 
-p <- ggplot(hom.ca, aes(Year, per, group = Country.or.Area,
+p <- ggplot(hom.ca.per, aes(Year, per, group = Country.or.Area,
                         color = Country.or.Area)) +
                           geom_line(size = 1.1) +
                           scale_y_continuous(labels = percent) +
-                          xlim(1995, 2017) +
+                          xlim(2000, 2015) +
                           ylab("homicide rate as a percentage of that in 2000") +
                           ggtitle("Homicide Rates in Central America as a Percentage of those in 2000")
 direct.label(p, "last.bumpup")
 ggsave("graphs/hom-ca-percentage.png", dpi = 100,
+       width = 9, height = 5)
+
+
+
+hom.ca.per7 <- subset(hom.ca, Year >= 2006)
+hom.ca.per7 <- ddply(hom.ca.per7, .(Country.or.Area), transform,
+      per = Rate / Rate[length(Rate)])
+
+p <- ggplot(hom.ca.per7, aes(Year, per, group = Country.or.Area,
+                        color = Country.or.Area)) +
+                          geom_line(size = 1.1) +
+                          scale_y_continuous(labels = percent) +
+                          xlim(2006, 2013) +
+                          ylab("homicide rate as a percentage of that in 2006") +
+                          ggtitle("Homicide Rates in Central America as a Percentage of those in 2006")
+direct.label(p, "last.bumpup")
+ggsave("graphs/hom-ca-percentage07.png", dpi = 100,
        width = 9, height = 5)
